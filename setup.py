e4b827e7f782d4baa44976b44e2a1c3813d81496
dromosys <android.robot.factory@gmail.com>
2018-07-17 14:29:56 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='opensolutionhomecredit',
-    version='1.0.4',
+    version='1.0.5',
     description='open-solution-home-credit',
     author='open-solution-home-credit',
     author_email='example@example.com',
     packages=['src'],  #same as name
     package_data={'src': ['kaggle.yaml']},
-    install_requires=['neptune-cli','steppy','attrdict','click'],
+    install_requires=['neptune-cli','steppy','attrdict','click','swifter'],
 )
