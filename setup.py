39fbb3c923cdd01e19cc94800e935766c54db828
dromosys <android.robot.factory@gmail.com>
2018-07-17 13:57:07 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     author='open-solution-home-credit',
     author_email='example@example.com',
     packages=['src'],  #same as name
+    package_data={'opensolutionhomecredit': ['neptune.yaml']},
     install_requires=['neptune-cli','steppy','attrdict','click'],
 )
