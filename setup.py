ceb69ee15f68e159e76ef32a82a32b6942d9e5ad
dromosys <android.robot.factory@gmail.com>
2018-07-17 13:33:56 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='opensolutionhomecredit',
-    version='1.0',
+    version='1.0.1',
     description='open-solution-home-credit',
     author='open-solution-home-credit',
     author_email='example@example.com',
     packages=['src'],  #same as name
+    install_requires=['neptune-cli','steppy','attrdict','click'],
 )
