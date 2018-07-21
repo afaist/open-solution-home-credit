6c74d4676a602f34b13cab6764b8501840dd563d
dromosys <android.robot.factory@gmail.com>
2018-07-21 18:55:44 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opensolutionhomecredit',
-    version='1.2.7',
+    version='1.2.8',
     description='open-solution-home-credit',
     author='open-solution-home-credit',
     author_email='example@example.com',
